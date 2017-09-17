87baa336e2415fcd2cc87ef9f6510967f2217ab7
Raphael Fakhreddine <raphaelfacredyn@gmail.com>
2017-09-17 16:41:16 -0700

    A couple changes
    Changes:
    * Now using categorical cross entropy because it is easier
    * Output layer now called predictions (predictions/Softmax)
    * Added categorical_accuracy metric

--- train_network.py
+++ train_network.py
@@ -5,6 +5,8 @@ from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
 import sys
 import time
+from keras.utils import plot_model
+
 
 def current_millis_time():
     return int(round(time.time() * 1000))
@@ -29,15 +31,15 @@ train_generator = train_datagen.flow_from_directory(
     'train_resized',  # this is the target directory
     target_size=(299, 299),  # all images will be resized to 299x299
     batch_size=batch_size,
-    class_mode='binary'
-)  # since we use binary_crossentropy loss, we need binary labels
+    class_mode='categorical'
+)  # since we use categorical_crossentropy loss, we need categorical labels
 
 # this is a similar generator, for validation data
 validation_generator = test_datagen.flow_from_directory(
     'validation_resized',
     target_size=(299, 299),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
 # create the base pre-trained model
 base_model = InceptionV3(weights='imagenet', include_top=False)
@@ -47,8 +49,8 @@ x = base_model.output
 x = GlobalAveragePooling2D()(x)
 # let's add a fully-connected layer
 x = Dense(1024, activation='relu')(x)
-# and an output layer with one neuron which is binary
-predictions = Dense(1, activation='softmax')(x)
+# and an output layer with one neuron for each category
+predictions = Dense(2, activation='softmax', name='predictions')(x)
 
 # this is the model we will train
 model = Model(inputs=base_model.input, outputs=predictions)
@@ -59,9 +61,13 @@ for layer in base_model.layers:
     layer.trainable = False
 
 # compile the model (should be done *after* setting layers to non-trainable)
-model.compile(optimizer='rmsprop', loss='binary_crossentropy')
+model.compile(
+    optimizer='rmsprop',
+    loss='categorical_crossentropy',
+    metrics=['categorical_accuracy'])
 
 if sys.argv[1] == "load":
+    print "Loading model+ " + sys.argv[2]
     model.load_weights(sys.argv[2])
 else:
     # train the model on the new data for a few epochs
@@ -71,17 +77,14 @@ else:
         epochs=8,
         validation_data=validation_generator,
         validation_steps=numValidationImgs // batch_size)
-    model.save('saves/trained_top_layers'+str(current_millis_time)+'.h5')
-    model.save_weights('saves/trained_top_layers-weights'+str(current_millis_time)+'.h5')
+    plot_model(model, to_file='preview/model.png', show_shapes=True)
+    model.save('out/top_layers-' + str(current_millis_time()) + '.h5')
+    model.save_weights('out/top_layers_weights-' +
+                       str(current_millis_time()) + '.h5')
 
 # at this point, the top layers are well trained and we can start fine-tuning
 # convolutional layers from inception V3. We will freeze the bottom N layers
 # and train the remaining top layers.
-
-# let's visualize layer names and layer indices to see how many layers
-# we should freeze:
-for i, layer in enumerate(base_model.layers):
-    print(i, layer.name)
 
 # we chose to train the top 2 inception blocks, i.e. we will freeze
 # the first 249 layers and unfreeze the rest:
@@ -93,16 +96,20 @@ for layer in model.layers[249:]:
 # we need to recompile the model for these modifications to take effect
 # we use SGD with a low learning rate
 model.compile(
-    optimizer=SGD(lr=0.0001, momentum=0.9), loss='binary_crossentropy')
+    optimizer=SGD(lr=0.0001, momentum=0.9),
+    loss='categorical_crossentropy',
+    metrics=['categorical_accuracy'])
 
+print "Starting Main Training"
 # we train our model again (this time fine-tuning the top 2 inception blocks
 # alongside the top Dense layers
 model.fit_generator(
     train_generator,
-    steps_per_epoch=(numTrainImgs // batch_size),
+    steps_per_epoch=numTrainImgs // batch_size,
     epochs=50,
     validation_data=validation_generator,
     validation_steps=numValidationImgs // batch_size)
 
-model.save('saves/trained_final'+str(current_millis_time)+'.h5')
-model.save_weights('saves/trained_final-weights'+str(current_millis_time)+'.h5')
+model.save('out/final-' + str(current_millis_time()) + '.h5')
+model.save_weights('out/final_weights-' + str(current_millis_time()) +
+                   '.h5')
